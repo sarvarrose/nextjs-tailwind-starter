17b52c29a1ad9e9e1a89ab1de54c7a45b7272ef6
Sarvar <sarvar.kumar@weareflamingo.in>
2021-04-01 15:57:38 +0530

    moved meta tags to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 import 'tailwindcss/tailwind.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="google-site-verification"
+          content="O4qtv_pMxTclZeLUKIlUhe6iQJ5gaSTknWk8PQZr1Eg"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Example Website" />
+        <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/_document.js
+++ pages/_document.js
@@ -5,20 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <meta charSet="utf-8" />
-        <meta
-          name="google-site-verification"
-          content="O4qtv_pMxTclZeLUKIlUhe6iQJ5gaSTknWk8PQZr1Eg"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Example Website" />
-        <link href="/favicon.ico" rel="icon" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
         <body>
           <Main />
           <NextScript />
